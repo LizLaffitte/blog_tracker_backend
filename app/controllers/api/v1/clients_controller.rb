07ecb53b2fb2a5d378a4022e63b9a1312725af32
lizlaffitte <laffitte.digital@gmail.com>
2021-02-18 21:17:53 -0500

    Create basic client controller actions to render correct json data.

---
+++ app/controllers/api/v1/clients_controller.rb
@@ -0,0 +1,42 @@
+class ClientsController < ApplicationController
+    before_action :find_client, only: [:show, :update, :destroy]
+    def create
+        client = current_user.clients.build(client_params)
+        if client.save
+            render json: ClientSerializer.new(client)
+        else
+            render json: {errors: "Client could not be created"}
+        end
+    end
+
+    def show
+        render json: ClientSerializer.new(@client)
+    end
+
+    def index
+        @clients = current_user.clients
+        render json: ClientSerializer.new(@clients)
+    end
+
+    def update
+        if @client.update(client_params)
+            render json: ClientSerializer.new(@client)
+        else
+            render json: {errors: "Cannot update client"}
+        end
+    end
+
+    def destroy
+        @client.destroy
+        render json: {message: "Client deleted."}
+    end
+
+    private
+    def client_params
+        params.require(:client).permit(:name, :website, :at_base, :at_table)
+    end
+
+    def find_client
+        @client = Client.find_by_id(params[:id])
+    end
+end
