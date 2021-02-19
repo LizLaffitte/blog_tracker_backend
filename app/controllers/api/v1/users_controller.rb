b670ea8458e93ba97eb4a0edc53b72f615049a0d
lizlaffitte <laffitte.digital@gmail.com>
2021-02-18 20:58:47 -0500

    Flesh out user controller actions.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,20 +1,33 @@
 class UsersController < ApplicationController
     wrap_parameters :user, include: [:username, :email, :password, :at_key]
-
+    before_action :find_user, only: [:show, :destroy, :update]
     def create
         user = User.new(user_params)
-        session[:user_id] = user.id
-        render json: UserSerializer.new(user)
+        if user.save
+            session[:user_id] = user.id
+            render json: UserSerializer.new(user)
+        else
+            render json: {errors: "Something went wrong"}
+        end
     end
 
     def show
-
+        if @user
+            render json: UserSerializer.new(@user)
+        end
     end
 
-    def edit
+    def update
+        if @user.update(user_params)
+            render json: UserSerializer.new(@user)
+        else
+            render json: {errors: "Something went wrong"}
+        end
     end
 
     def destroy
+        @user.destroy
+        render json: {message: "User deleted."}
 
     end
 
@@ -22,5 +35,9 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:username, :email, :password, :at_key)
+    end
+
+    def find_user
+        @user = User.find_by_id(params[:id])
     end
 end
