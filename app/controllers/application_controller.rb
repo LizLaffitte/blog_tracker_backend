8e3a6d8fe67d53a065610225f28e91ec38da3107
lizlaffitte <laffitte.digital@gmail.com>
2021-02-20 10:07:22 -0500

    Add module so we can use helper methods in api only app.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
+    include ActionController::Helpers
     helper_method :current_user
+
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
     end
