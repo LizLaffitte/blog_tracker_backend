4cdff33241f7344b5d25fe886f5901d1370bc30d
lizlaffitte <laffitte.digital@gmail.com>
2021-02-20 10:08:56 -0500

    Plan for website param issues.

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::BlogsController < ApplicationController
     def index
         client_site = params[:website]
-        
-        response = HTTParty.get(client_site + "/wp-json/wp/v2/posts")
-        render json: response.body
+        if client_site
+            response = HTTParty.get(client_site + "/wp-json/wp/v2/posts")   
+            render json: response.body
+        else
+            render json: {errors: "No website supplied."}
+        end
     end
 end
