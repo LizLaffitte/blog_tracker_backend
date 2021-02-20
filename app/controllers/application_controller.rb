e7284686d2ed08446f05f71ace8a25a93c89d7d9
lizlaffitte <laffitte.digital@gmail.com>
2021-02-20 09:39:51 -0500

    Implement helper methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::API
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
     end
+
+
+    def logged_in?
+        !!session[:user_id]
+    end
 end
