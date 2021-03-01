e42a02ec713f06ff39427427bb42aec531288dbb
lizlaffitte <laffitte.digital@gmail.com>
2021-02-28 19:24:22 -0500

    Overwriting from_token_request based on how we find users in Users model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :clients
+
+
+    def self.from_token_request(request)
+        User.find_by(username: request.params[:user_token][:auth][:username])
+    end
+
 end
