921b5f76bab8f78c077c4b36ea7f56ad3cc0f137
lizlaffitte <laffitte.digital@gmail.com>
2021-02-28 19:22:18 -0500

    Controller for user token.

---
+++ app/controllers/api/v1/user_token_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::UserTokenController < Knock::AuthTokenController
+skip_before_action :verify_authenticity_token, raise:false
+    private
+    def auth_params
+        params.require(:auth).permit(:username, :password)
+    end
+end
