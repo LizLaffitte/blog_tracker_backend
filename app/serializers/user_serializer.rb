506bb5b5d40da18cbacf660263d7467de5fc2267
lizlaffitte <laffitte.digital@gmail.com>
2021-03-06 13:22:38 -0500

    Add a user's client data to User serializer so we only have to make one API call.:

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,14 @@
 class UserSerializer
   include JSONAPI::Serializer
   attributes :username, :email, :at_key
+  attribute :clients do | user |
+    user.clients do |client|
+      {
+        id: client.id,
+        name: client.name,
+        base: client.at_base, 
+        table: client.at_table
+      }
+    end
+  end
 end
